7a28172d5b26f0008f7ef47d7509666a16d4b2df
Warwickc3295 <31700743+Warwickc3295@users.noreply.github.com>
2017-09-06 11:15:02 -0400

    Add files via upload

---
+++ M2T1_Warwick.py
@@ -0,0 +1,12 @@
+#CTI110
+#M2T1 - Sales Prediction
+#8/30
+#Warwick
+
+# Ask for total sales
+totalSales = int(input('Please input the Total Sales: '))
+                    
+# Calculate profit
+annualProfit = 0.23 * totalSales
+# Print the profit amount
+print('The annual profit is',annualProfit)
